ae89b38dc5bd29373baaf0c2b6b852c69136cecc
Mike M <mikal815@users.noreply.github.com>
2018-06-20 21:26:12 -0400

    created function which loops over the topics array and creates buttons

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-    
+    console.log("working");
     var topics = ['guitars','cadillacs','hyenas','the singularity','surfing','street art','abandoned places'];
 
+    var mkbtn = function(){
+        $("#topics").empty();
 
+        for (var i = 0; i < topics.length; i++) {
+
+            var button = $("<btn>");
+        }
+
+
+    }
 
 
 
