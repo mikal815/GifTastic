6e1ad12ff789de155cd9677de49a3797ef694a6c
Mike M <mikal815@users.noreply.github.com>
2018-06-20 21:10:53 -0400

    html setup and made topics array

---
+++ assets/javascript/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    
+    var topics = ['guitars','cadillacs','hyenas','the singularity','surfing','street art','abandoned places'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
